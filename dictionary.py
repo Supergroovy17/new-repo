2dc08d9de11f5160da4dd106959d44b54aef56cc
Supergroovy17 <166076637+Supergroovy17@users.noreply.github.com>
2024-04-22 11:53:23 -0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,58 @@
+
+
+#Your task is to update this menu based on given instructions. This exercise tests your ability to manipulate nested dictionaries and manage data effectively.
+
+Starters={"Soup": 5.99, "Bruschetta": 6.50},
+Main_Course= {"Steak": 15.99, "Salmon": 13.99}
+Desserts= {"Cake": 4.99, "Ice Cream": 3.99}
+
+restaurant_menu={'Starters' : Starters,
+                 'Main_Course' : Main_Course,
+                 'Desserts' : Desserts}
+
+restaurant_menu = {
+    "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
+    "Main_Course": {"Steak": 15.99, "Salmon": 13.99},
+    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
+}
+
+
+restaurant_menu ['Beverages']=  'Man-hatten'" : "'10.00', 'Oldfashiond' ' : ' '9.00'
+
+Main_Course ['Steak']=17.99 # how do I get this to replace steak 
+print(Main_Course)
+restaurant_menu.pop({"Soup"}) #.pop not working the way I want it to 
+print(restaurant_menu)
+
+
+
+#Task 1: Customer Service Ticket Tracker Demonstrate your ability to use nested collections and loops by creating a system to track customer service tickets.
+
+#Problem Statement: Develop a program a function that:
+
+#Tracks customer service tickets, each with a unique ID, customer name, issue description, and status (open/closed).
+#Implement functions to:
+#Open a new service ticket.
+#Update the status of an existing ticket.
+#Display all tickets or filter by status.
+#Initialize with some sample tickets and include functionality for additional ticket entry.
+#Example ticket structure:
+
+service_tickets = {
+    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
+    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
+}
+
+
+#def new_service ():
+   #.update
+
+
+#def update_status():
+
+#.update
+#def show_tickets():
+    #print(service_tickets)
+
+#def show_status():
+    #print(Status)
